b2f791ae9bcafc80d16246d462e426b0b924f1cb
loveritsu929Home <loveritsu929@gmail.com>
2018-03-26 21:04:10 -0400

    UserBean: add attribute hashedPassword

--- src/bean/UserBean.java
+++ src/bean/UserBean.java
@@ -6,28 +6,39 @@ import java.util.Date;
 public class UserBean {
 	private int uid;  //AUTO
 	private String uname;
+	private String hashedPassword;
 	private Timestamp ulastactive; //timestamp
 	
 	
 	
-	public UserBean(int id, String name, Timestamp timeStamp) {
+	public UserBean(int id, String name, String Password, Timestamp timeStamp) {
 		this.uid= id;
 		this.uname = name;
+		this.hashedPassword = Password.hashCode() + "";
 		this.ulastactive = timeStamp;
 	}
 	
-	public UserBean(String name, Timestamp timeStamp) {
+	public UserBean(String name, String Password, Timestamp timeStamp) {
 		this.uname = name;
+		this.hashedPassword = Password.hashCode() + "";
 		this.ulastactive = timeStamp;
 	}
 	
 	@Override
 	public String toString() {
 		return String.format(
-				"UserBean [userId=%s, userName=%s, lastActive=%s]",
-				uid, uname, ulastactive.toString());
+				"UserBean [userId=%s, userName=%s, userHashedPassword=%s lastActive=%s]",
+				uid, uname, hashedPassword, ulastactive.toString());
 	}
 	
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
+
+	public void setHashedPassword(String hashedPassword) {
+		this.hashedPassword = hashedPassword;
+	}
+
 	public int getUid() {
 		return uid;
 	}
