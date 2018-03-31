d00e04adc768b3ef08f4d3a424f61fd40b5a7f96
loveritsu929Home <loveritsu929@gmail.com>
2018-03-31 19:38:14 -0400

    OrderBean

--- src/bean/OrderBean.java
+++ src/bean/OrderBean.java
@@ -1,9 +1,60 @@
 package bean;
 
+import java.sql.*;
+
 public class OrderBean {
-
-	public OrderBean() {
-		// TODO Auto-generated constructor stub
+	//oid, ouid, otime, ostatus, oprice
+	private int id;
+	private int userId;
+	private Timestamp timestamp;
+	private String status;
+	private int totalPrice;
+	
+	
+	
+	public OrderBean(int userId, Timestamp timestamp, String status, int totalPrice) {
+		super();
+		this.userId = userId;
+		this.timestamp = timestamp;
+		this.status = status;
+		this.totalPrice = totalPrice;
 	}
-
+	
+	@Override
+	public String toString() {
+		return "OrderBean [id=" + id + ", userId=" + userId + ", timestamp=" + timestamp + ", status=" + status
+				+ ", totalPrice=" + totalPrice + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
+	
 }
