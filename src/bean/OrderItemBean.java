ad73f342d3e0f013163471a19b57595512f2c91a
loveritsu929Home <loveritsu929@gmail.com>
2018-03-31 19:38:31 -0400

    OrderItemBean

--- src/bean/OrderItemBean.java
+++ src/bean/OrderItemBean.java
@@ -1,9 +1,57 @@
 package bean;
 
 public class OrderItemBean {
+	//oiid, oioid, oibid, oiamount
+	//private int id;
+	private int orderId;
+	private int bookId;
+	private int amount;
+	private int price;
 
-	public OrderItemBean() {
-		// TODO Auto-generated constructor stub
+	
+	public OrderItemBean(int orderId, int bookId, int amount) {
+		super();
+		this.orderId = orderId;
+		this.bookId = bookId;
+		this.amount = amount;
 	}
+
+	public OrderItemBean(int orderId, int bookId, int amount, int price) {
+		super();
+		this.orderId = orderId;
+		this.bookId = bookId;
+		this.amount = amount;
+		this.price = price;
+	}
+	
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
 
 }
