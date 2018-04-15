bf8e54ecb3966e0e10256b9779fed809d9c5e2df
masterchan <masterchan.256@gmail.com>
2018-04-15 10:49:29 -0400

    Front-end Design Upload

---
+++ WebContent/js/master.js
@@ -0,0 +1,41 @@
+(function(){
+	document.addEventListener("DOMContentLoaded", function(event) { 
+
+		// Navigation Bar
+		function check_position(){
+  			var body = document.getElementsByTagName("BODY")[0];
+  			var nav = document.getElementById("menu");
+  			if (window.scrollY > 200){
+  				body.classList.add("floating-nav");
+  				nav.style="top:0";
+  			}else{
+				body.classList.remove("floating-nav");
+				nav.style="top:-50px";
+  			}
+		}
+
+		window.onscroll = function(){
+			check_position();
+		};
+		check_position();
+
+    // Push to bottom
+    function check_height(){
+      var main = document.getElementById("main-body");
+      if (main == null){return;}
+
+      var footer = document.getElementById("footer");
+      if (footer == null){return;}
+
+      var delta = document.body.scrollHeight - main.clientHeight - footer.clientHeight - 2;
+      if (delta > 0){
+        main.style = "min-height:" + (delta + main.clientHeight) + "px";
+      }
+    }
+
+    window.onresize = function(){
+      check_height();
+    }
+    check_height();
+	});
+})();
