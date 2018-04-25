edc169db3f9c390217c241d8766201adfd3933e2
vishal <vishal.malik0027@gmail.com>
2018-04-25 18:27:51 -0400

    Done Doc of AddressBean

--- src/bean/AddressBean.java
+++ src/bean/AddressBean.java
@@ -1,5 +1,7 @@
 package bean;
-
+/**
+ * 	An object to Store Shipping Address.
+ */
 public class AddressBean {
 	//said, sauid, satime, safirst, salast, sastreet, saprovince, sacountry, sazip
 	//private int id;
@@ -30,50 +32,102 @@ public class AddressBean {
 		this.country = country;
 		this.zip = zip;
 	}
-	
+	/**
+	 * Returns the User Id
+	 * @return userId
+	 */
 	public int getUserId() {
 		return userId;
 	}
+	/**
+	 * Set the User Id
+	 * @param userId
+	 */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+	/**
+	 * Returns the First Name
+	 * @return firstname
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
+	/**
+	 * Sets the First Name
+	 * @param firstName
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	/**
+	 * Returns the Last Name
+	 * @return lastName
+	 */
 	public String getLastName() {
 		return lastName;
 	}
+	/**
+	 * Sets the Last Name
+	 * @param lastName
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	/**
+	 * Returns the Street Address
+	 * @return street
+	 */
 	public String getStreet() {
 		return street;
 	}
+	/**
+	 * Set the Street Address
+	 * @param street
+	 */
 	public void setStreet(String street) {
 		this.street = street;
 	}
+	/**
+	 * Returns the Province
+	 * @return province
+	 */
 	public String getProvince() {
 		return province;
 	}
+	/**
+	 * Set the Province
+	 * @param province
+	 */
 	public void setProvince(String province) {
 		this.province = province;
 	}
+	/**
+	 * Returns the country
+	 * @return country
+	 */
 	public String getCountry() {
 		return country;
 	}
+	/**
+	 * Sets the country
+	 * @param country
+	 */
 	public void setCountry(String country) {
 		this.country = country;
 	}
+	/**
+	 * Returns the ZIP (Postal Code)
+	 * @return zip
+	 */
 	public String getZip() {
 		return zip;
 	}
+	/**
+	 * Sets the postal code (ZIP)
+	 * @param zip
+	 */
 	public void setZip(String zip) {
 		this.zip = zip;
-	}
-	
-	
-	
+	}	
 }
