c5b4c51142df035efb73b17c9d16f1f6470be8c5
vishal <vishal.malik0027@gmail.com>
2018-04-25 20:12:56 -0400

    UserBean Doc Done

--- src/bean/UserBean.java
+++ src/bean/UserBean.java
@@ -8,6 +8,9 @@ import java.sql.Timestamp;
 import java.util.Random;
 
 public class UserBean {
+	/**
+	 * Declarations of Private Fields
+	 */
 	private int uid; // AUTO
 	private String userName;
 	private String hashedPassword;
@@ -16,6 +19,13 @@ public class UserBean {
 	private String role;
 
 	// create an user from sign up
+	/**
+	 * Creates New User object and Stores it. 
+	 * @param name
+	 * @param password
+	 * @throws UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 */
 	public UserBean(String name, String password) throws UnsupportedEncodingException, NoSuchAlgorithmException {
 		Random random = new Random();
 		this.userName = name;
@@ -24,6 +34,15 @@ public class UserBean {
 	}
 
 	// get an user in database
+	/**
+	 * gets an user in database
+	 * @param id
+	 * @param name
+	 * @param password
+	 * @param salt
+	 * @param timeStamp
+	 * @param role
+	 */
 	public UserBean(Integer id, String name, String password, String salt, Timestamp timeStamp, String role) {
 		this.uid = id;
 		this.userName = name;
@@ -32,16 +51,32 @@ public class UserBean {
 		this.lastActive = timeStamp;
 		this.role = role;
 	}
-
+	/**
+	 * Verifies the User Password 
+	 * @param password
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 */
 	public boolean verifyPassword(String password) throws UnsupportedEncodingException, NoSuchAlgorithmException {
 		return this.hashedPassword.equals(hashPassword(password, this.salt));
 	}
-	
+	/**
+	 * Hashes the Password with SHA-256 and Salt
+	 * @param password
+	 * @param salt
+	 * @return hashedPassword
+	 * @throws UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 */
 	public static String hashPassword(String password, String salt) throws UnsupportedEncodingException, NoSuchAlgorithmException {
 	    MessageDigest digest = MessageDigest.getInstance("SHA-256");
 	    return new String(digest.digest((password + salt).getBytes("UTF-8")), StandardCharsets.UTF_8);
 	}
-
+	/**
+	 * Tester 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// Testing
 		try {
@@ -52,53 +87,87 @@ public class UserBean {
 			e.printStackTrace();
 		}
 	}
-	
-
-
+	/**
+	 * Returns UID
+	 * @return
+	 */
 	public int getUid() {
 		return uid;
 	}
-
+	/**
+	 * Sets UID
+	 * @param uid
+	 */
 	public void setUid(int uid) {
 		this.uid = uid;
 	}
-
+	/**
+	 * Returns User Name
+	 * @return userName
+	 */
 	public String getUserName() {
 		return userName;
 	}
-
+	/**
+	 * Sets User Name
+	 * @param userName
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
+	/**
+	 * Returns Hassed Password
+	 * @return hashedPassword
+	 */
 	public String getHashedPassword() {
 		return hashedPassword;
 	}
-
+	/**
+	 * Sets the Hashed Password
+	 * @param hashedPassword
+	 */
 	public void setHashedPassword(String hashedPassword) {
 		this.hashedPassword = hashedPassword;
 	}
-
+	/**
+	 * Returns the Salt 
+	 * @return salt
+	 */
 	public String getSalt() {
 		return salt;
 	}
-
+	/**
+	 * Sets the Salt
+	 * @param salt
+	 */
 	public void setSalt(String salt) {
 		this.salt = salt;
 	}
-
+	/**
+	 * Returns the Last Active Time Stamp
+	 * @return lastActive
+	 */
 	public Timestamp getLastActive() {
 		return lastActive;
 	}
-
+	/**
+	 * Sets the Last Active Time stamp
+	 * @param lastActive
+	 */
 	public void setLastActive(Timestamp lastActive) {
 		this.lastActive = lastActive;
 	}
-
+	/**
+	 * Returns the Role
+	 * @return role
+	 */
 	public String getRole() {
 		return role;
 	}
-
+	/**
+	 * Sets the Role
+	 * @param role
+	 */
 	public void setRole(String role) {
 		this.role = role;
 	}
